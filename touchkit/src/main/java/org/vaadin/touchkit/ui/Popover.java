e92a7681294488ee6ce526eef8d64a17b2a5e258
Daniel Nordhoff-Vergien <dve@vergien.net>
2017-03-09 10:24:58 +0100

    Make Popover v8 component

--- touchkit/src/main/java/org/vaadin/touchkit/ui/Popover.java
+++ touchkit/src/main/java/org/vaadin/touchkit/ui/Popover.java
@@ -5,7 +5,7 @@ import org.vaadin.touchkit.gwt.client.vcom.popover.PopoverState;
 
 import com.vaadin.ui.Component;
 import com.vaadin.ui.ComponentContainer;
-import com.vaadin.v7.ui.VerticalLayout;
+import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
 
 /**
